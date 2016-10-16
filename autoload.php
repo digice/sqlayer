9244e0c30b8137c8fe805404e694d815858e91f3
Roderic Linguri <digices@mp5.local>
2016-10-16 12:12:11 -0600

    Prevent attempt to load abstract base classes file

--- autoload.php
+++ autoload.php
@@ -6,9 +6,6 @@ require_once(__DIR__.DIRECTORY_SEPARATOR.'etc'.DIRECTORY_SEPARATOR.'config.php')
 /** Load Vendor libraries **/
 require_once(__DIR__.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php');
 
-/** Load Abstract Base Classes **/
-require_once(__DIR__.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'.abc.php');
-
 /** Load Library Classes **/
 $di = new DirectoryIterator(__DIR__.DIRECTORY_SEPARATOR.'lib');
 foreach ($di as $item) {
