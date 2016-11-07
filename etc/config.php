bdcdd53b8ebeff178db4ed5ba9ce2683d75d8e17
Roderic Linguri <linguri@digices.com>
2016-11-07 04:52:08 -0700

    Add comments, remove constant definitions

--- etc/config.php
+++ etc/config.php
@@ -1,21 +1,15 @@
 <?php
 
+/**
+ * @package SQLayer
+ * @version 0.0.3
+ * @date    2016-11-08
+ * @author  Roderic Linguri <linguri@digices.com>
+ * @license MIT
+ */
+
+/** Main Configuration File **/
+
 if (!defined('APP_DIR')) {
   define('APP_DIR',dirname(__DIR__));
-}
-
-if (!defined('DB_HOST')) {
-  define('DB_HOST','127.0.0.1');
-}
-
-if (!defined('DB_NAME')) {
-  define('DB_NAME','sqlayer_test');
-}
-
-if (!defined('DB_USER')) {
-  define('DB_USER','sqlayer_test');
-}
-
-if (!defined('DB_PASS')) {
-  define('DB_PASS','sqlayer_test');
 }
